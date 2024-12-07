5ebcde96afb46cf25bd787272722ac5e89b17097
arnav <arnabdas2753@gmail.com>
2024-12-07 13:08:09 +0530

    Responsive Navbar with Sidebar Toggle X

--- src/components/ui/navbar.tsx
+++ src/components/ui/navbar.tsx
@@ -3,7 +3,7 @@
 import { useState, useEffect } from "react";
 import Image from "next/image";
 import NavLink from "../shared";
-import { AlignJustify } from "lucide-react";
+import { AlignJustify, X } from "lucide-react";
 import { cn } from "~/lib/utils";
 import Link from "next/link";
 
@@ -16,7 +16,6 @@ const Navbar = () => {
   useEffect(() => {
     const handleScroll = () => {
       const currentScrollPos = window.scrollY;
-
       setVisible(prevScrollPos > currentScrollPos || currentScrollPos < 10);
       setPrevScrollPos(currentScrollPos);
     };
@@ -30,10 +29,12 @@ const Navbar = () => {
       <div
         className={cn(
           "inset-0 smooth-transition z-[500] md:hidden px-[24px] py-[100px] block bg-black/80 backdrop-blur-md fixed",
-          openSideBar ? "translate-x-0" : "-translate-x-full",
+          openSideBar ? "translate-x-0" : "-translate-x-full"
         )}
       >
-        <p className="text-[60px] font-bold">Navigate</p>
+        <div className="flex justify-between items-center">
+          <p className="text-[60px] font-bold">Navigate</p>
+        </div>
         <div>
           <ul className="pt-[40px]">
             {links.map((link, index) => {
@@ -43,7 +44,7 @@ const Navbar = () => {
                   key={link}
                   className={cn(
                     "text-[20px] fade-in-blur-1 py-[20px]",
-                    index !== links.length - 1 && "border-b border-neutral-700",
+                    index !== links.length - 1 && "border-b border-neutral-700"
                   )}
                 >
                   <Link href={`#${link.split(" ").join("").toLowerCase()}`}>
@@ -58,20 +59,37 @@ const Navbar = () => {
 
       <nav
         className={cn(
-          `w-full text-white ${openSideBar ? "bg-transparent" : "bg-black/50"} fixed z-[1000] top-0 flex justify-between items-center lg:px-[40px] px-[20px] transition-a duration-300 ease-in-out`,
+          `w-full text-white ${
+            openSideBar ? "bg-transparent" : "bg-black/50"
+          } fixed z-[1000] top-0 flex justify-between items-center lg:px-[40px] px-[20px] transition-a duration-300 ease-in-out`,
           visible
-            ? `md:h-[130px] h-[80px] md:backdrop-blur-none ${openSideBar ? "backdrop-blur-none bg-transparent" : "backdrop-blur-md "}`
-            : `md:h-[80px] h-[80px] ${openSideBar ? "bg-transparent" : "bg-black/15  backdrop-blur-md "}`,
+            ? `md:h-[130px] h-[80px] md:backdrop-blur-none ${
+                openSideBar
+                  ? "backdrop-blur-none bg-transparent"
+                  : "backdrop-blur-md "
+              }`
+            : `md:h-[80px] h-[80px] ${
+                openSideBar
+                  ? "bg-transparent"
+                  : "bg-black/15  backdrop-blur-md "
+              }`
         )}
       >
         <div className="max-w-[1440px] w-full flex justify-between items-center mx-auto">
           <div className="flex fade-in-blur-left-0 cursor-pointer items-center gap-[5px]">
-            <AlignJustify
-              type="button"
-              onClick={() => setOpenSideBar((prev) => !prev)}
-              className="md:hidden block"
-              fontWeight={900}
-            />
+            {openSideBar ? (
+              <X
+                type="button"
+                onClick={() => setOpenSideBar(false)}
+                className="md:hidden block text-white w-8 h-8 cursor-pointer transition-all duration-200"
+              />
+            ) : (
+              <AlignJustify
+                type="button"
+                onClick={() => setOpenSideBar(true)}
+                className="md:hidden block text-white w-8 h-8 cursor-pointer"
+              />
+            )}
 
             <Link href="#">
               <Image
@@ -81,7 +99,7 @@ const Navbar = () => {
                 alt="keizer-brand-logo"
                 className={cn(
                   "md:w-[175px] md:h-[79.82px] sm:w-[135px] sm:h-[58.856px] w-[80.4px] h-[49.83px] transition-all",
-                  visible ? "scale-100" : "md:scale-[0.9] scale-100",
+                  visible ? "scale-100" : "md:scale-[0.9] scale-100"
                 )}
               />
             </Link>
@@ -101,7 +119,6 @@ const Navbar = () => {
                 );
               })}
             </ul>
-
             <Link
               href="#contact"
               className="font-sans flex flex-center bg-white hover:bg-neutral-300 text-black w-[102px] h-[36px] font-[700] text-[16px] rounded-[4px]"
